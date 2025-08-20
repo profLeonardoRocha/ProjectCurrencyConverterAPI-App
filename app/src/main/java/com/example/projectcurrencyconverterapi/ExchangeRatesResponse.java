cede2196d2164178689136ff3fea18f1e5be7602
Leonardo Rocha <leonardossr@gmail.com>
2025-08-20 09:34:00 -0300

    Cria arquivos para funcionamento da aplicação

---
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRatesResponse.java
@@ -0,0 +1,6 @@
+// Modelo de dados (DTO - Data Transfer Object) que representa a resposta
+// json da API
+package com.example.projectcurrencyconverterapi;
+
+public class ExchangeRatesResponse {
+}
