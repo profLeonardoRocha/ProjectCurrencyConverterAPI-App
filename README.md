6db1ad7a94eb2fc5d7f70cf9f11a69633514c16c
Leonardo Rocha <leonardossr@gmail.com>
2025-08-22 12:01:58 -0300

    adiciona referência à lib retrofit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Projeto de conversor de Moeda usando API
+
+
+### Referências
+
+[Biblioteca Retrofit](https://square.github.io/retrofit/)
--- app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
+++ app/src/main/java/com/example/projectcurrencyconverterapi/ExchangeRateService.java
@@ -1,5 +1,14 @@
-// Interface que identifica o endpoint da API que será consumida pelo Retrofit
+// Interface que identifica o endpoint da API que será
+// consumida pelo Retrofit
 package com.example.projectcurrencyconverterapi;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface ExchangeRateService {
+    /**
+     * Método para obter taxas de câmbio recentes
+     * **/
+
 }
